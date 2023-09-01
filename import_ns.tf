e3cd609e7d02542a4c452cf12dea256b9b121fdb
Michal Kuritka <kuritka@gmail.com>
2023-09-01 16:49:22 +0200

    1 - review generated

--- import_ns.tf
+++ import_ns.tf
@@ -4,9 +4,9 @@ import {
 }
 import {
   to = kubernetes_namespace.us_east_1b
-  id = "us-east-1a"
+  id = "us-east-1b"
 }
 import {
   to = kubernetes_namespace.us_east_1c
-  id = "us-east-1a"
+  id = "us-east-1c"
 }
--- main.tf
+++ main.tf
@@ -1,7 +1,30 @@
-#
-#resource "kubernetes_namespace" "us_east_1a" {
-#  metadata {
-#    name = "us-east-1a"
-#  }
-#}
+locals {
+  common_annotations = {
+    "kuritka.io/repo"     = "tf-associate"
+    "kuritka.io/provider" = "k8s"
+  }
+}
 
+resource "kubernetes_namespace" "us_east_1c" {
+  metadata {
+    annotations = local.common_annotations
+    labels      = {}
+    name        = "us-east-1c"
+  }
+}
+
+resource "kubernetes_namespace" "us_east_1b" {
+  metadata {
+    annotations = local.common_annotations
+    labels      = {}
+    name        = "us-east-1b"
+  }
+}
+
+resource "kubernetes_namespace" "us_east_1a" {
+  metadata {
+    annotations = local.common_annotations
+    labels      = {}
+    name        = "us-east-1a"
+  }
+}
